05f2d5f09a070bb0539ec65cdc4340a4e6595c24
Iyke Israel <iykeprince48@gmail.com>
2022-08-10 22:54:51 +0100

    update cred

--- .circleci/ansible/roles/configure-server/files/myenv.sh
+++ .circleci/ansible/roles/configure-server/files/myenv.sh
@@ -1,9 +1,9 @@
 export: TYPEORM_CONNECTION=postgres
 export: TYPEORM_ENTITIES=./src/moudles/domain/**/*.entity.ts 
-export: TYPEORM_HOST=udapeopledb.cswpijcayaw1.us-east-1.rds.amazonaws.com
+export: TYPEORM_HOST=postgres.cogtefobggcs.us-east-1.rds.amazonaws.com
 export: TYPEORM_PORT=5432
 export: TYPEORM_USERNAME=postgres
-export: TYPEORM_PASSWORD=!Pass4sure
+export: TYPEORM_PASSWORD=1234567890
 export: TYPEORM_DATABASE=udapeopledb
 export: TYPEORM_MIGRATIONS=/src/migrations/*.ts
 export: TYPEORM_MIGRATIONS_DIR=/src/migrations/
